185da1a2ca41d23b081d6518b65fd7a85ada27da
Deophius <NO1zhuce@163.com>
2023-12-12 17:40:35 +0800

    Made the cmd window invisible on Windows.

--- src/main.cpp
+++ src/main.cpp
@@ -4,8 +4,16 @@
 #include <nana/gui/widgets/menubar.hpp>
 #include <nana/gui.hpp>
 
+#if __has_include(<windows.h>)
+// We're on windows
+#include <windows.h>
+#endif
+
 int main() {
     using namespace Reversi;
+    #if __has_include(<windows.h>)
+    ::ShowWindow(::GetConsoleWindow(), SW_HIDE);
+    #endif
     MainWindow mw("board.bmp");
     mw.newgame_dialog(true);
     mw.show();
